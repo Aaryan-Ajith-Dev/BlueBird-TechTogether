4eb96caab3a4cbc16638cef159674e6464bdcadf
SiddharthPalod <75942243+SiddharthPalod@users.noreply.github.com>
2023-08-05 16:35:16 +0530

    Add files via upload

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,10 @@
 import shoe from "./shoe.png"
-import qrcode from "./qrcode.png"
 import loginbg from "./loginbg.png";
 import bgimg from "./bgimg.png";
 
 
 export{ 
     shoe,
-    qrcode,
     loginbg,
     bgimg,
 };
